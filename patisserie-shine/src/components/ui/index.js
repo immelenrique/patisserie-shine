5323f3a8f8eb315504598e972e23cd852bcc87f1
immelenrique <40810980+immelenrique@users.noreply.github.com>
2025-08-17 01:10:43 +0200

    Update index.js

--- patisserie-shine/src/components/ui/index.js
+++ patisserie-shine/src/components/ui/index.js
@@ -1,5 +1,5 @@
-// src/components/ui/index.js
 export { Card } from './Card';
 export { StatCard } from './StatCard';
 export { StatusBadge } from './StatusBadge';
 export { Modal } from './Modal';
+
